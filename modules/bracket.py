bab9ef849187aa10a5f954cacf551f37d1c51c2b
Ksenia <bondarks42@gmail.com>
2022-11-10 20:40:21 +0300

    Add bracket.py

---
+++ modules/bracket.py
@@ -0,0 +1,30 @@
+def bracketFunction():
+    brackets= input('Введите скобочную последовательность:')
+    stack = []
+    is_good = True
+
+    for i in brackets:
+        if i in '({[':
+            stack.append(i)
+        elif i in ')}]':
+            if not stack:
+                is_good = False
+                break
+            open_bracket = stack.pop()
+            if open_bracket == '(' and i ==')':
+                continue
+            if open_bracket == '[' and i ==']':
+                continue
+            if open_bracket == '{' and i =='}':
+                continue
+            is_good = False
+            break
+
+    if is_good and len(stack) == 0:
+        print('Yes')
+    else:
+        print('No')
+
+# Дана скобочная последовательность: [((())()(())]], она считается неправильной, нужно удалить лишние открывающую и закрывающую скобки: [(())()(())]
+
+
--- program.py
+++ program.py
@@ -1,8 +1,9 @@
 from modules.hello import helloFunction
 from modules.name import nameFunction
 from modules.numbers import numbersFunction
-
+from modules.bracket import bracketFunction
 
 helloFunction()
 nameFunction()
 numbersFunction()
+bracketFunction()
